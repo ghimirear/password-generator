92ae8560c63fc3743b8d8ca13d8291ff3e3e76f0
Arjun Ghimire <ghimirearjun15@gmail.com>
2020-11-25 15:09:36 -0500

    final pushes

--- script.js
+++ script.js
@@ -1,90 +1,75 @@
- // Assignment Code
-//console.log("app connecting")
+//accessing Html file
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
-// Write password to the #password input
+// add event listner to generateBtn
+generateBtn.addEventListener("click", writePassword);
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-    generatePassword();
-  });
-
-
-//setting up variables.
+//setting up  global variables.
 var numbersStrings = "1234567890".split("");
-console.log(numbersStrings);
+//console.log(numbersStrings);
 var lowerString = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(lowerString);
+//console.log(lowerString);
 
 var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-console.log(upperString);
+//console.log(upperString);
 
-var specialString = "!@#$^&*(>)_%-:;+=~`".split("");
-console.log(specialString);
-
-var userOptions = [];
-
-console.log(numbersStrings);
-console.log(lowerString);
-console.log(upperString);
-console.log(specialString);
+var specialString = "!@#$^&*(><,)_%-:/;+=~`".split("");
+//console.log(specialString);
 
 function generatePassword(){ 
   
-  var passLength = prompt("please enter the length of your password")
+  var passLength = prompt("please enter the length of your password between 8 nad 128");
  
   passLength = parseInt(passLength);
-  if (passLength >= 8 && passLength <=128){
-
+  //eliminating possible erros to get the required length of password
+  while (passLength < 8 || passLength > 128 || isNaN(passLength) || passLength <0 ){
+    alert("your password must be between 8 and 128");{
+      passLength = prompt("please enter the length of your password between 8 and 128");
+   }
+  }
+  //letting the user to choose any of these options.
     var numbers = confirm("do you want numbers in your password?");
-    if (numbers){
-      userOptions = userOptions.concat(numbersStrings);
-      console.log(userOptions);
-    }
-    
-   
     var special = confirm( "do you want specials characters in your password?");
-    if (special){
-      userOptions = userOptions.concat(specialString);
-      console.log(userOptions);
-    }
-    
-    
     var lower = confirm( " do you want lowecase characters in your password?");
-    if (lower){
-      userOptions = userOptions.concat(lowerString);
-      console.log(userOptions);
+    var upper = confirm("do you want Uppercase character in your password?");
+   
+  //if above conditions  not chosen then 
+  while (numbers === false && special === false && lower === false && upper === false){
+    alert("you need to selectatleast one optios!!!!!!");
 
+    numbers = confirm("do you want numbers in your password?");
+    special = confirm( "do you want specials characters in your password?");
+    lower = confirm( " do you want lowecase characters in your password?");
+    upper = confirm("do you want Uppercase character in your password?");
   }
-  
- 
-  var upper = confirm("do you want Uppercase character in your password?");
-  if (upper){
+  //checking user choices and joining them to array of password.
+  var userOptions = [];
+
+  if ( numbers === true){
+    userOptions = userOptions.concat(numbersStrings);
+  }
+  if ( special === true){
+    userOptions = userOptions.concat(specialString);
+  }
+  if ( lower === true){
+    userOptions = userOptions.concat(lowerString);
+  }
+  if ( upper === true){ 
     userOptions = userOptions.concat(upperString);
-    console.log(userOptions);
   }
-  console.log( "user options are");
- console.log(userOptions); 
-  
+  console.log(userOptions);
 
-
-var generatedPassword ="";
- for ( var i =0; i < passLength; i++){
-  var rand = Math.floor(Math.random() * userOptions.length 
- );
- generatedPassword +=userOptions[rand];
- console.log(generatedPassword);
-} 
-console.log(generatedPassword);
-password = generatedPassword;
-var passwordText = document.querySelector("#password");
-passwordText.textContent = password;
-}
-else{
-  alert("it must be inbetween 8 and 128")
-  passwordText.textContent = "";
-}
+  //creating an empty string to store generated password naming generatedPassword.
+  var generatedPassword ="";
+  // creating an for loop to generate random characters from user choices.
+    for ( var i =0; i < passLength; i++){
+    generatedPassword = generatedPassword + userOptions[Math.floor(Math.random() * userOptions.length)];
+    console.log(generatedPassword);
+  } 
+return generatedPassword;
+} //setting up the password to display.
+  function writePassword(){
+    var password = generatePassword();
+    passwordText.value = password;
   }
 
